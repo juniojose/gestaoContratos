2e994829c3043b90c834532101a1d931abc10ab4
Junio Santos - SLC Agrícola <junio.santos@slcagricola.com.br>
2024-11-15 15:00:54 -0300

    Removendo o arquivo check_status.py que não é mais necessário

--- check_status.py
+++
@@ -1,9 +0,0 @@
-from app import create_app, db
-from app.models import Status
-
-app = create_app()
-
-with app.app_context():
-    statuses = Status.query.all()
-    for status in statuses:
-        print(f"ID: {status.statusId}, Descrição: {status.statusDescricao}, Data de Cadastro: {status.statusDataCadastro}, Data de Última Atualização: {status.statusDataUltimaAtualizacao}")
