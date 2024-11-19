70f86318a04bd080563fa178967b4d672a43a5f8
juniojose <juniojose@gmail.com>
2024-11-19 05:43:40 -0300

    Atualização do models.py: inclusão das tabelas usuarios e usuariosPerfis

--- app/models.py
+++ app/models.py
@@ -28,3 +28,35 @@ class Fazenda(db.Model):
 
     # Relacionamento com Status
     status = db.relationship('Status', backref=db.backref('fazendas', lazy=True))
+
+class UsuarioPerfil(db.Model):
+    __tablename__ = 'usuariosPerfis'
+
+    perfilId = db.Column(db.Integer, primary_key=True)
+    statusId = db.Column(db.Integer, db.ForeignKey('status.statusId'), nullable=False)
+    perfilNome = db.Column(db.String(50), unique=True, nullable=False)
+    perfilDescricao = db.Column(db.String(100), nullable=True)
+    perfilDataCadastro = db.Column(db.DateTime, default=datetime.utcnow)
+    perfilDataUltimaAtualizacao = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Relacionamento com Status
+    status = db.relationship('Status', backref=db.backref('usuariosPerfis', lazy=True))
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+
+    usuarioId = db.Column(db.Integer, primary_key=True)
+    statusId = db.Column(db.Integer, db.ForeignKey('status.statusId'), nullable=False)
+    fazendaId = db.Column(db.Integer, db.ForeignKey('fazendas.fazendaId'), nullable=True)
+    usuarioPerfilId = db.Column(db.Integer, db.ForeignKey('usuariosPerfis.perfilId'), nullable=False)
+    usuarioNome = db.Column(db.String(150), nullable=False)
+    usuarioEmail = db.Column(db.String(255), unique=True, nullable=False)
+    usuarioSenha = db.Column(db.String(255), nullable=False)
+    usuarioDataValidadePermissao = db.Column(db.DateTime, nullable=True)
+    usuarioDataCadastro = db.Column(db.DateTime, default=datetime.utcnow)
+    usuarioDataUltimaAtualizacao = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Relacionamentos
+    status = db.relationship('Status', backref=db.backref('usuarios', lazy=True))
+    fazenda = db.relationship('Fazenda', backref=db.backref('usuarios', lazy=True))
+    perfil = db.relationship('UsuarioPerfil', backref=db.backref('usuarios', lazy=True))
